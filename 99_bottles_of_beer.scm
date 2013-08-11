5d92fe1d511835910162dce0eb3fc39a4b3902df
Adei Josol <helena.josol@gmail.com>
2013-08-11 02:56:46 +0100

    Simplify

--- 99_bottles_of_beer.scm
+++ 99_bottles_of_beer.scm
@@ -1,50 +1,15 @@
-(define drink-all-the-beer!! ; Start singing!
-  (lambda ()
-    (we-still-got-loads! 99)))
-
-(define we-still-got-loads!
-  (lambda (beer)
-    (start-downing-one!)
-    (how-many-bottles?! beer)
-    (how-many-again?! beer)
+(define (99-bottles-of-beer)
+  (define (bottle-number beer)
+    (if (= beer 0) "No" (number->string beer)))
+  (define (bottle-word beer)
+    (if (= beer 1) " bottle" " bottles"))
+  (define (bottles-of-beer beer)
+    (display (string-append (bottle-number beer) (bottle-word beer) " of beer on the wall\n"))
+    (display (string-append (bottle-number beer) (bottle-word beer) " of beer\n"))
     (cond
-      ((no-more-beer...? beer)
-       (buy-more!!!)
-       (how-many-we-got?! 99))
-      (else
-        (drink-it!)
-        (how-many-left?! (just-downed-one! beer))
-        (we-still-got-loads! (just-downed-one! beer))))))
-
-(define how-many-bottles?!
-  (lambda (beer)
-    (sing! (slurrr (i-can-count beer) " bottles of beer on the wall"))))
-
-(define how-many-again?!
-  (lambda (beer)
-    (sing! (slurrr (i-can-count beer) " bottles of beer"))))
-
-(define buy-more!!!
-  (lambda ()
-    (sing! "Go to the store and buy some more")))
-
-(define drink-it!
-  (lambda ()
-    (sing! "Grab one down and pass it around")))
-
-(define how-many-we-got?! how-many-bottles?!)
-(define how-many-left?! how-many-bottles?!)
-
-(define sing!
-  (lambda (drunken-line)
-    (splurt. (slurrr drunken-line (breathe)))))
-
-(define breathe (lambda () "\n"))
-(define just-downed-one! (lambda (beer) (down-it! beer 1)))
-
-(define start-downing-one! newline)
-(define i-can-count number->string)
-(define slurrr string-append)
-(define splurt. display)
-(define no-more-beer...? zero?)
-(define down-it! -)
+      ((zero? beer) (display "Go to the store and buy some more\n")
+                    (display "99 bottles of beer on the wall\n"))
+      (else (display "Grab one down and pass it around\n")
+            (display (string-append (bottle-number (- beer 1)) (bottle-word (- beer 1)) " of beer on the wall\n\n"))
+            (bottles-of-beer (- beer 1)))))
+  (bottles-of-beer 99))
